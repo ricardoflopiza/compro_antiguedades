022fa723cb21478a8fddb6db06841319bab4c4fa
Ricardo Pizarro <rik990@gmail.com>
2025-04-28 12:47:02 -0400

    corrigiendo rutas6

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,22 +3,13 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
-// En prod Gh-Pages uso la subruta, en dev sirvo en /
-const BASE = import.meta.env.PROD
-  ? "/compro_antiguedades/" // <-- la carpeta donde publicas en GitHub Pages
-  : "/"; // <-- en local sirvo siempre desde la raíz
-
 export default defineConfig({
-  // tu dominio custom en prod
-  site: import.meta.env.PROD
-    ? "https://www.comproantiguedades.cl"
-    : "http://localhost:4322",
-
-  base: BASE,
+  // apúntalo a tu dominio custom
+  site: "https://comproantiguedades.cl",
+  // sin subcarpeta, ¡va en la raíz!
+  base: "/",
   integrations: [tailwind(), icon()],
-
   server: {
-    // para que Astro pueda hacer new URL(...) en dev
-    origin: "http://localhost:4322",
+    origin: "http://localhost:4321",
   },
 });
